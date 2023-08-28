15812a411c02bd0b1fa04abd3caab634517243d9
Aris Empanta <eams220891@gmail.com>
2023-08-28 20:51:40 +0300

    created a basic class based view for home page

--- base_app/views.py
+++ base_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 # Create your views here.
+class Home(ListView):
+    template_name = 'home.html'
+    
+    def get_queryset(self):
+        # Return the data you want to display as a list or queryset
+        custom_data = [
+            {'title': 'Item 1', 'content': 'Content 1'},
+            {'title': 'Item 2', 'content': 'Content 2'},
+            # Add more items as needed
+        ]
+        return custom_data
