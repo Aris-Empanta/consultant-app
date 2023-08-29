1b2b583a6f61c289186bc428dd5044acf968d159
Aris Empanta <eams220891@gmail.com>
2023-08-29 19:14:28 +0300

    modularizing templates to components

--- base_app/views.py
+++ base_app/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views import View
 
 # Create your views here.
-class Home(ListView):
-    template_name = 'home.html'
+class Home(View):
+    def get(self, request):
+        return render(request, 'components/home.html')
     
-    def get_queryset(self):
-        # Return the data you want to display as a list or queryset
-        custom_data = [
-            {'title': 'Item 1', 'content': 'Content 1'},
-            {'title': 'Item 2', 'content': 'Content 2'},
-            # Add more items as needed
-        ]
-        return custom_data
+class Login(View):
+    def post(self, request):
+        pass
