5e563e1d7401d5ee766af88fe199ddd5439f208d
Aris Empanta <eams220891@gmail.com>
2023-08-30 19:10:58 +0300

    working on auth backend

--- base_app/urls.py
+++ base_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.Home.as_view())
+    path('', views.Home.as_view(), name="home"),
+    path('login/', views.Login.as_view(), name="login")
 ]
--- base_app/views.py
+++ base_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate
+
 
 # Create your views here.
 class Home(View):
@@ -7,5 +11,17 @@ class Home(View):
         return render(request, 'components/home.html')
     
 class Login(View):
+    def get(self, request):
+        return render(request, 'components/login.html')
+    
     def post(self, request):
-        pass
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.error(request, 'Email OR password does not exit')
+            return
+
+        user = authenticate(email=email, password=password)
