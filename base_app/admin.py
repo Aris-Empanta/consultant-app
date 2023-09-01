63da6779f5a9b1269b7802a8b890cb82ef58e571
Aris Empanta <eams220891@gmail.com>
2023-09-01 19:25:45 +0300

    modified tables

--- base_app/admin.py
+++ base_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Lawyers
+from .models import Lawyer, Client, Profile, Rating
    
-admin.site.register(Lawyers)
+admin.site.register(Lawyer)
+admin.site.register(Client)
+admin.site.register(Profile)
+admin.site.register(Rating)
--- base_app/models.py
+++ base_app/models.py
@@ -3,31 +3,22 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, blank=False, on_delete=models.CASCADE)
-    lawyer = models.BooleanField(default=False)
+    isLawyer = models.BooleanField(default=False)
 
-# Create your models here.
-class Lawyers(models.Model):
-    #instead of modifying the user model, we connnect this model to it.
+class Lawyer(models.Model):
     profile = models.OneToOneField(Profile, blank=False, on_delete=models.CASCADE, default=None)
     specialties = models.TextField(blank=False, null=False)
     description = models.TextField()
     averageRating = models.IntegerField()
     hourlyRate = models.IntegerField(blank=False, null=False)
-
-    def __str__(self):
-        return f"Lawyer Named: {self.name}" 
     
-class Clients(models.Model):
+class Client(models.Model):
     name = models.CharField(max_length=50, blank=False, null=False)
     surname = models.CharField(max_length=50, blank=False, null=False)
     email = models.EmailField(unique=True, blank=False, null=False)
-    #instead of modifying the user model, we connnect this model to it.
     profile = models.OneToOneField(Profile, blank=False, on_delete=models.CASCADE, default=None)
-    
-    def __str__(self):
-        return f"Client Named: {self.name}" 
 
-class Ratings(models.Model):
-    client = models.OneToOneField(Clients, on_delete=models.PROTECT)
-    lawyer = models.OneToOneField(Lawyers, on_delete=models.PROTECT)
+class Rating(models.Model):
+    client = models.OneToOneField(Client, on_delete=models.PROTECT)
+    lawyer = models.OneToOneField(Lawyer, on_delete=models.PROTECT)
     value = models.IntegerField(blank=False, null=False)
