81ffde3fde62516b30a226d59e4e7c6865c2bfb6
Aris Empanta <eams220891@gmail.com>
2023-10-10 17:48:30 +0300

    Adding appointment durations and breaks / working on template

--- base_app/viewsFiles/registerLawyer.py
+++ base_app/viewsFiles/registerLawyer.py
@@ -96,83 +96,86 @@ class LawyerAvailableHours(View):
         # csrf token and keep only the dates in the dictionary 
         available_hours_dict = request.POST.copy()
         available_hours_dict.pop('csrfmiddlewaretoken')
+        print(available_hours_dict)
 
-        # We loop though all the dates that have lawyer's available hours, in order
-        # to validate the times given and save them to the database.
-        for key, values in available_hours_dict.lists():
-            # We convert the current examined date from this string format "dd/mm/yyyy" 
-            # to 3 different integer variables: year, month, day, in order to construct 
-            # the datetime objects later successfully, and after the validation save them 
-            # to the database.
-            day_month_year_list = [ int(x) for x in key.split("/") ]
-            year = day_month_year_list[2]
-            month = day_month_year_list[1]
-            day = day_month_year_list[0]            
+        # # We loop though all the dates that have lawyer's available hours, in order
+        # # to validate the times given and save them to the database.
+        # for key, values in available_hours_dict.lists():
+        #     # We convert the current examined date from this string format "dd/mm/yyyy" 
+        #     # to 3 different integer variables: year, month, day, in order to construct 
+        #     # the datetime objects later successfully, and after the validation save them 
+        #     # to the database.
+        #     day_month_year_list = [ int(x) for x in key.split("/") ]
+        #     year = day_month_year_list[2]
+        #     month = day_month_year_list[1]
+        #     day = day_month_year_list[0]            
             
-            #We create datetime objects for the starting and ending time of the interval
-            starting_time_hours_and_minutes = [int(x) for x in values[0].split(":")]
-            ending_time_hours_and_minutes = [int(x) for x in values[1].split(":")]
+        #     #We create datetime objects for the starting and ending time of the interval
+        #     starting_time_hours_and_minutes = [int(x) for x in values[0].split(":")]
+        #     ending_time_hours_and_minutes = [int(x) for x in values[1].split(":")]
             
-            starting_time_1_hours = starting_time_hours_and_minutes[0]
-            starting_time_1_minutes = starting_time_hours_and_minutes[1]
-            ending_time_1_hours = ending_time_hours_and_minutes[0]
-            ending_time_1_minutes = ending_time_hours_and_minutes[1]
+        #     starting_time_1_hours = starting_time_hours_and_minutes[0]
+        #     starting_time_1_minutes = starting_time_hours_and_minutes[1]
+        #     ending_time_1_hours = ending_time_hours_and_minutes[0]
+        #     ending_time_1_minutes = ending_time_hours_and_minutes[1]
 
-            starting_time_1 = datetime(year, month, day, starting_time_1_hours, starting_time_1_minutes)
-            ending_time_1 = datetime(year, month, day, ending_time_1_hours, ending_time_1_minutes)
+        #     starting_time_1 = datetime(year, month, day, starting_time_1_hours, starting_time_1_minutes)
+        #     ending_time_1 = datetime(year, month, day, ending_time_1_hours, ending_time_1_minutes)
 
-            # The ending time should be at least 1 hour after the starting time, 
-            # otherwise it should return an error message
-            ending_one_hour_later = starting_time_1 + timedelta(hours=1)
+        #     # The ending time should be at least 1 hour after the starting time, 
+        #     # otherwise it should return an error message
+        #     ending_one_hour_later = starting_time_1 + timedelta(hours=1)
 
-            if ending_one_hour_later > ending_time_1:
-                error_message = "Ending times should be at least 1 hour after starting times"
-                messages.error(request, error_message)
-                return redirect('lawyer_available_hours')
-
-            #Now we handle the case the intervals in a day to be 2.
-            if(len(values) == 4):
-                starting_time_hours_and_minutes_2 = [int(x) for x in values[2].split(":")]
-                ending_time_hours_and_minutes_2 = [int(x) for x in values[3].split(":")]
-                
-                starting_time_2_hours = starting_time_hours_and_minutes_2[0]
-                starting_time_2_minutes = starting_time_hours_and_minutes_2[1]
-                ending_time_2_hours = ending_time_hours_and_minutes_2[0]
-                ending_time_2_minutes = ending_time_hours_and_minutes_2[1]
-
-                starting_time_2 = datetime(year, month, day, starting_time_2_hours, starting_time_2_minutes)
-                ending_time_2 = datetime(year, month, day, ending_time_2_hours, ending_time_2_minutes)
-
-                # The second interval's starting time should not be before the first interval's ending time.
-                if(starting_time_2 < ending_time_1):
-                    error_message = """ 
-                                        The second interval's starting time should be at the same time or 
-                                        later from the starting interval's ending time
-                                    """
-                    messages.error(request, error_message)
-                    return redirect('lawyer_available_hours')
-                
-                # The second intervals ending time should be at least one hour after the starting time.
-                ending_2_one_hour_later = starting_time_2 + timedelta(hours=1)
-
-                if ending_2_one_hour_later > ending_time_2:
-                    error_message = "Ending times should be at least 1 hour after starting times"
-                    messages.error(request, error_message)
-                    return redirect('lawyer_available_hours')
-                
-                # If the second interval's starting time is the same as the first interval's ending time, 
-                # we combine the 2 intervals in one, and empty the starting and ending time of the second 
-                # interval variables.
-                if(starting_time_2 == ending_time_1):
-                    ending_time_1 = ending_time_2
-                    starting_time_2 = None
-                    ending_time_2 = None
+        #     if ending_one_hour_later > ending_time_1:
+        #         error_message = "Ending times should be at least 1 hour after starting times"
+        #         messages.error(request, error_message)
+        #         return redirect('lawyer_available_hours')
             
-            #save them to the database the final intervals. Might be 2 or 1 if combined.
-            print(starting_time_1)
-            print(ending_time_1)
-            if starting_time_2 is not None:
-                print(starting_time_2)
-                print(ending_time_2)
+        #     # We initialise the starting and ending time of a potential second interval
+        #     starting_time_2 = None
+        #     ending_time_2 = None
+
+        #     #Now we handle the case the intervals in a day to be 2.
+        #     if(len(values) == 4):
+        #         starting_time_hours_and_minutes_2 = [int(x) for x in values[2].split(":")]
+        #         ending_time_hours_and_minutes_2 = [int(x) for x in values[3].split(":")]
+                
+        #         starting_time_2_hours = starting_time_hours_and_minutes_2[0]
+        #         starting_time_2_minutes = starting_time_hours_and_minutes_2[1]
+        #         ending_time_2_hours = ending_time_hours_and_minutes_2[0]
+        #         ending_time_2_minutes = ending_time_hours_and_minutes_2[1]
+
+        #         starting_time_2 = datetime(year, month, day, starting_time_2_hours, starting_time_2_minutes)
+        #         ending_time_2 = datetime(year, month, day, ending_time_2_hours, ending_time_2_minutes)
+
+        #         # The second interval's starting time should not be before the first interval's ending time.
+        #         if(starting_time_2 < ending_time_1):
+        #             error_message = """ 
+        #                                 The second interval's starting time should be at the same time or 
+        #                                 later from the starting interval's ending time
+        #                             """
+        #             messages.error(request, error_message)
+        #             return redirect('lawyer_available_hours')
+                
+        #         # The second intervals ending time should be at least one hour after the starting time.
+        #         ending_2_one_hour_later = starting_time_2 + timedelta(hours=1)
+
+        #         if ending_2_one_hour_later > ending_time_2:
+        #             error_message = "Ending times should be at least 1 hour after starting times"
+        #             messages.error(request, error_message)
+        #             return redirect('lawyer_available_hours')
+                
+        #         # If the second interval's starting time is the same as the first interval's ending time, 
+        #         # we combine the 2 intervals in one, and empty the starting and ending time of the second 
+        #         # interval variables.
+        #         if(starting_time_2 == ending_time_1):
+        #             ending_time_1 = ending_time_2
+        #             starting_time_2 = None
+        #             ending_time_2 = None
+            
+        #     #save them to the database the final intervals. Might be 2 or 1 if combined.
+            
+        #     if starting_time_2 is not None:
+        #         pass
 
         return redirect('lawyer_available_hours')
