822acc990e5b51fd86a0510d495a3fec9693104d
Aris Empanta <eams220891@gmail.com>
2023-10-23 19:31:41 +0300

    added links on lawyer's editable profile for available hours and appointments

--- static/js/lawyerProfile.js
+++ static/js/lawyerProfile.js
@@ -2,14 +2,42 @@ authenticatedLawyerInfo = document.getElementById('authenticatedLawyerInfo')
 editLawyerInfoForm = document.getElementById('editLawyerInfoForm')
 editLawyerInfoButton = document.getElementById('editLawyerInfoButton')
 cancelUpdatingLawyerInfo = document.getElementById('cancelUpdatingLawyerInfo')
+lawyerInfoLink = document.getElementById('lawyerInfoLink')
+lawyerAvailableHoursLink = document.getElementById('lawyerAvailableHoursLink')
+lawyerAppointmentsLink = document.getElementById('lawyerAppointmentsLink')
+lawyerInfoWrapper = document.getElementById('lawyerEditableInfoWrapper')
+availableHoursWrapper = document.getElementById('editableLawyerAvailableHoursWrapper')
+appointmentsWrapper = document.getElementById('lawyerAppointmentsWrapper')
 
 editLawyerInfoButton.addEventListener('click', () => {
     authenticatedLawyerInfo.style.display = 'none'
     editLawyerInfoForm.style.display = 'flex'
+    lawyerAvailableHoursLink.disabled = true
+    lawyerAppointmentsLink.disabled = true
 })
 
 cancelUpdatingLawyerInfo.addEventListener('click', (e) => {
     e.preventDefault()
     editLawyerInfoForm.style.display = 'none'
     authenticatedLawyerInfo.style.display = 'initial'
+    lawyerAvailableHoursLink.disabled = false
+    lawyerAppointmentsLink.disabled = false
+})
+
+lawyerInfoLink.addEventListener('click', () => {
+    lawyerInfoWrapper.style.display = 'initial'
+    availableHoursWrapper.style.display = 'none'
+    appointmentsWrapper.style.display = 'none'
+})
+
+lawyerAvailableHoursLink.addEventListener('click', () => {
+    lawyerInfoWrapper.style.display = 'none'
+    availableHoursWrapper.style.display = 'initial'
+    appointmentsWrapper.style.display = 'none'
+})
+
+lawyerAppointmentsLink.addEventListener('click', () => {
+    lawyerInfoWrapper.style.display = 'none'
+    availableHoursWrapper.style.display = 'none'
+    appointmentsWrapper.style.display = 'initial'
 })
