365c22fa0fbdb9c5bafb9337457973c4742f1163
Aris Empanta <eams220891@gmail.com>
2023-11-30 21:09:52 +0200

    completed delete account styling

--- static/js/deleteAccount.js
+++ static/js/deleteAccount.js
@@ -4,7 +4,7 @@ const deleteAccountButton  = document.getElementById('deleteAccountButton')
 
 deleteAccountButton.addEventListener('click', async () => {
 
-    deleteAccountButton.style.backgroundColor = 'green'
+    deleteAccountButton.style.backgroundColor = 'rgb(105, 53, 156, 0.7)'
     deleteAccountButton.innerHTML = 'Please Wait'
 
     try {
@@ -25,14 +25,14 @@ deleteAccountButton.addEventListener('click', async () => {
 
         // Check if the response status is OK (status code 200)
         if (!response.ok) {
-            deleteAccountButton.style.backgroundColor = 'red'
+            deleteAccountButton.style.backgroundColor = '#EF0107'
             deleteAccountButton.innerHTML = 'Delete Account'
             return alert('A Network error occured, please try again later.');
         }
         const data = await response.json()
 
         if(data.deleted) {
-            deleteAccountButton.style.backgroundColor = 'blue'
+            deleteAccountButton.style.backgroundColor = '#4286A8'
             deleteAccountButton.innerHTML = 'Account Deleted!'
 
             setTimeout(() => {
@@ -41,12 +41,11 @@ deleteAccountButton.addEventListener('click', async () => {
             return 
         }
 
-        deleteAccountButton.style.backgroundColor = 'red'
+        deleteAccountButton.style.backgroundColor = '#EF0107'
         deleteAccountButton.innerHTML = 'Delete Account'
 
         alert('A Network error occured, please try again later.');
     } catch (error) {
-        console.log(error)
         // Handle any errors that occur during the fetch or response handling (change it in production)
         alert('An unexpected error occured, please try again later');
     }
