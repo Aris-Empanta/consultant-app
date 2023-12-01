e4dbbb89f985df2b756de1e3f6da65b6e40832b8
Aris Empanta <eams220891@gmail.com>
2023-12-01 20:27:47 +0200

    working on search lawyer page pagination

--- base_app/template_views/lawyers_search_results.py
+++ base_app/template_views/lawyers_search_results.py
@@ -3,12 +3,14 @@ from django.views import View
 from ..models import Lawyer
 from django.db.models import Q
 from ..base_classes.lawyers import BaseLawyer
+from django.core.paginator import Paginator
 
 class LawyersSearchResults(View, BaseLawyer):
     def get(self, request):
         # These query params should exist otherwise we send the 404 page.
         expertise = request.GET.get('expertise')
         name = request.GET.get('name')
+        page = request.GET.get("page")
 
         if expertise == None or name==None:
             return render(request, 'components/reusable/404.html')
@@ -49,8 +51,40 @@ class LawyersSearchResults(View, BaseLawyer):
             lawyer_info['areas_of_expertise'] = lawyer.areasOfExpertise.split(':')
 
             lawyers_data.append(lawyer_info)
+        
+        paginator = Paginator(lawyers_data, 4)
+        page_of_lawyers = paginator.get_page(page)
 
-        context = { 'lawyers_data': lawyers_data, 
-                    'lawyers_amount': len(lawyers_data) }
+        # The variable that hold the queries except of page number, 
+        # to assist the pagination links generation.
+        base_queries = f'?expertise={expertise}&name={name}&page='
+        current_page_obj = paginator.page(page)
+
+        # The pages numbers
+        previous_page = current_page_obj.previous_page_number() if current_page_obj.has_previous() else None
+        next_page = current_page_obj.next_page_number() if current_page_obj.has_next() else None
+        last_page = paginator.num_pages if paginator.num_pages > 1 else None
+
+        # The pages links
+        previous_page_link = f'{base_queries}{previous_page}' if previous_page else None
+        current_page_link = f'{base_queries}{page}'
+        next_page_link = f'{base_queries}{next_page}' if next_page else None
+        last_page_link = f'{base_queries}{last_page}' if last_page else None
+
+        context = { 
+                    'lawyers_data': page_of_lawyers, 
+                    'lawyers_amount': len(lawyers_data),
+                    'base_queries': base_queries,
+                    'paginator': paginator,
+                    'current_page_obj': current_page_obj,
+                    'previous_page': previous_page,
+                    'previous_page_link': previous_page_link,
+                    'next_page': next_page,
+                    'next_page_link': next_page_link,
+                    'last_page': last_page,
+                    'last_page_link' : last_page_link,
+                    'current_page': int(page),
+                    'current_page_link': current_page_link,
+                   }
 
         return render(request, 'components/lawyers_search_results.html', context)
