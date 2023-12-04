69fda4ed4b43246123cafa6d0790c33948bc5b4b
Aris Empanta <eams220891@gmail.com>
2023-12-04 19:06:45 +0200

    Configuring for production

--- consultant_app/asgi.py
+++ consultant_app/asgi.py
@@ -6,11 +6,14 @@ from channels.auth import AuthMiddlewareStack
 from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'consultant_app.settings')
+# We initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
 from base_app.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket':AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     )
