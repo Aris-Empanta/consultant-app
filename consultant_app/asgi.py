85c7b1fc56b2c1698daaa62305deca91a4144b0a
Aris Empanta <eams220891@gmail.com>
2023-12-04 19:01:13 +0200

    Configuring for production

--- consultant_app/asgi.py
+++ consultant_app/asgi.py
@@ -4,9 +4,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-from base_app.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'consultant_app.settings')
+
+from base_app.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
