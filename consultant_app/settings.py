695b085861cb1ac42dfacb20814e89747b6eb869
Aris Empanta <eams220891@gmail.com>
2023-12-06 20:45:46 +0200

    working on password reset bug

--- consultant_app/settings.py
+++ consultant_app/settings.py
@@ -27,11 +27,12 @@ CORS_ORIGINS_WHITELIST = ['http://localhost:8000', 'https://lawyer-appointment-a
 # The email sending mechanism configuration
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv("EMAIL_HOST")  
+EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_USE_TLS = True 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_DEBUG = True
 
 INSTALLED_APPS = [
     'daphne',
